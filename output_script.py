13ba33bb888ce69d801b927e5d2e88251cabdb16
alicehodson <73225020+alicehodson@users.noreply.github.com>
2021-03-23 11:36:00 +0000

    Update output_script.py
    Runs and times kraken2 on the fasta files to create output files

--- output_script.py
+++ output_script.py
@@ -5,7 +5,7 @@ def reads_output(file_name, size):
     with open(file_name, "r") as fh:
        subprocess.Popen(f'time kraken2 --db /home/mbyah11/kraken2-microbial-fatfree/ --use-names --threads 4 {file_name} --output zymo_{size}_run_{sample_letter}.output'.split()).communicate()
 
-sample_letter = '0'
+sample_letter = 'n'
 reads_output('zymo_log_4000_reads_0.fastq', 'OG')
 reads_output('zymo_short_100_0.fasta', '100')
 reads_output('zymo_short_200_0.fasta', '200')
