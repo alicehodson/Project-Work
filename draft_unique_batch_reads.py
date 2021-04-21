820bc37ab03303bc71ff0857c1a559af119057f2
alicehodson <73225020+alicehodson@users.noreply.github.com>
2021-04-21 18:28:51 +0100

    Update draft_unique_batch_reads.py

--- draft_unique_batch_reads.py
+++ draft_unique_batch_reads.py
@@ -6,13 +6,16 @@ output_files = list(directory.rglob('*'))
 output_files = [fastq_string_file for fastq_string_file in output_files if '.fastq' in fastq_string_file.suffixes]
 print(output_files)
 
+
+num_reads = [3000, 3512, 3768, 3896, 3960]
+reads_list = []
+count = 0
 idx = [0,3000], [3000,3512], [3512,3768], [3768,3896], [3896,3960]
 for idx,(name,seq,qual,comment) in enumerate(pyfastx.Fastx('soil_sample_a.fastq')):
     count += 1
     reads_list.append(f">{name}\n{seq}")
     if count == num_reads:
-      count = 0
       with open(f'{num_reads}_short_{read_file.name}', 'w') as fh:
-        fh.write("\n".join(reads_list))
+          fh.write("\n".join(reads_list))
       reads_list = []
       break
