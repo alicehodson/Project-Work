019c0f3a94a3dba7b748ff3fba9622ab57e826f1
alicehodson <73225020+alicehodson@users.noreply.github.com>
2021-04-22 11:05:46 +0100

    Update draft_unique_batch_reads.py

--- draft_unique_batch_reads.py
+++ draft_unique_batch_reads.py
@@ -7,22 +7,16 @@ output_files = [fastq_string_file for fastq_string_file in output_files if '.fas
 print(output_files)
 
 
-def unique_read(num_reads, file_number):
-    reads_list = []
-    count = 0
-    sample_letter = 'a'
-    idx = [0,3000], [3000,3512], [3512,3768], [3768,3896], [3896,3960]
-    for idx,(name,seq,qual,comment) in enumerate(pyfastx.Fastx('soil_sample_a.fastq')):
-        count += 1
-        reads_list.append(f">{name}\n{seq}")
-        if count == num_reads:
-            with open(f'{file_number}_short_unique_{sample_letter}.fastq', 'w') as fh:
-                fh.write("\n".join(reads_list))
-            reads_list = []
-            break
+reads_list = []
+count = 0
+sample_letter = 'a'
+idx = [0, 3000], [3000, 3512], [3512, 3768], [3768, 3896], [3896, 3960]
+for idx,(name,seq,qual,comment) in enumerate(pyfastx.Fastx('soil_sample_a.fastq')):
+    count += 1
+    reads_list.append(f">{name}\n{seq}")
+    if count in [3000, 3512, 3768, 3896, 3960]:
+        with open(f'{count}_short_unique_{sample_letter}.fastq', 'w') as fh:
+            fh.write("\n".join(reads_list))
+        reads_list = []
+        break
 
-unique_read(3000, '3000')
-unique_read(3512, '512')
-unique_read(3768, '256')
-unique_read(3896, '128')
-unique_read(3960, '64')
